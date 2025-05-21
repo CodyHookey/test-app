a88a06e2b9575191a5658ec40a1efa4a08eba690
Cody Hookey <codyhookey@outlook.com>
2025-05-21 12:07:10 +0100

    added fix for post creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @post = Post.create(content: params[:content])
+    @post = Post.create(content: params[:content], user_id: params[:user_id])
 
     render json: {post: @post}
   end
